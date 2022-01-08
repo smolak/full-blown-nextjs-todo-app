9a54e9da05b308af2d4fc339700aecad3b4dda3b
Jacek Smolak <jacek.smolak@gmail.com>
2022-01-08 21:48:40 +0100

    Add todo view component

---
+++ todo/ui/todo.spec.tsx
@@ -0,0 +1,165 @@
+import { screen, render, fireEvent, waitFor } from "@testing-library/react";
+import { Todo, TodoProps } from "./todo";
+import userEvent from "@testing-library/user-event";
+import { taskDone, taskNotDone, tasks } from "../fixtures/tasks.fixture";
+
+const defaultProps: TodoProps = {
+  onFormSubmit: () => Promise.resolve(taskDone),
+  tasks,
+  onTaskClick: () => Promise.resolve(taskDone),
+};
+const getInputField = () => screen.getByPlaceholderText("What do you have to do?");
+
+describe("Todo", () => {
+  describe("form", () => {
+    it("should have empty input field initially", () => {
+      render(<Todo {...defaultProps} />);
+
+      const inputField = getInputField();
+
+      expect(inputField).toHaveValue("");
+    });
+
+    it("should have input field focused by default", () => {
+      render(<Todo {...defaultProps} />);
+
+      expect(getInputField()).toHaveFocus();
+    });
+
+    describe("when a task is typed and 'enter' is pressed", () => {
+      it("should submit a task", () => {
+        const props = {
+          ...defaultProps,
+          onFormSubmit: jest.fn(),
+        };
+        render(<Todo {...props} />);
+
+        userEvent.type(getInputField(), "First task{enter}");
+
+        expect(props.onFormSubmit).toHaveBeenCalledWith("First task");
+      });
+
+      it("should clear the form", async () => {
+        render(<Todo {...defaultProps} />);
+
+        userEvent.type(getInputField(), "First task{enter}");
+
+        await waitFor(() => {
+          expect(getInputField()).toHaveValue("");
+        });
+      });
+
+      it("should keep the focus on input field", () => {
+        render(<Todo {...defaultProps} />);
+
+        userEvent.type(getInputField(), "First task{enter}");
+
+        expect(getInputField()).toHaveFocus();
+      });
+    });
+
+    describe("when a task is typed and form is submitted using the submit button", () => {
+      it("should submit a task", () => {
+        const props = {
+          ...defaultProps,
+          onFormSubmit: jest.fn(),
+        };
+        render(<Todo {...props} />);
+
+        userEvent.type(getInputField(), "First task");
+        userEvent.click(screen.getByRole("button", { name: /add/i }));
+
+        expect(props.onFormSubmit).toHaveBeenCalledWith("First task");
+      });
+
+      it("should clear the form", async () => {
+        render(<Todo {...defaultProps} />);
+
+        userEvent.type(getInputField(), "First task");
+        userEvent.click(screen.getByRole("button", { name: /add/i }));
+
+        await waitFor(() => {
+          expect(getInputField()).toHaveValue("");
+        });
+      });
+
+      it("should keep the focus on input field", async () => {
+        render(<Todo {...defaultProps} />);
+
+        userEvent.type(getInputField(), "First task");
+        userEvent.click(screen.getByRole("button", { name: /add/i }));
+
+        await waitFor(() => {
+          expect(getInputField()).toHaveFocus();
+        });
+      });
+    });
+  });
+
+  describe("list", () => {
+    it("should be empty if no tasks are passed", () => {
+      const props = {
+        ...defaultProps,
+        tasks: [],
+      };
+      render(<Todo {...props} />);
+
+      expect(screen.getByRole("list")).toBeInTheDocument();
+      expect(screen.queryAllByRole("listitem")).toHaveLength(0);
+    });
+
+    it("should render tasks if passed", () => {
+      const props = {
+        ...defaultProps,
+        tasks,
+      };
+      render(<Todo {...props} />);
+
+      expect(screen.getByRole("list")).toBeInTheDocument();
+      expect(screen.queryAllByRole("listitem")).toHaveLength(props.tasks.length);
+    });
+
+    it("should render not done tasks with checkbox not checked", () => {
+      const props = {
+        ...defaultProps,
+        tasks: [taskNotDone],
+      };
+      const taskNotDoneId = props.tasks[0].id;
+
+      render(<Todo {...props} />);
+      const checkboxOfTaskNotDone = screen.getByTestId(taskNotDoneId).querySelector('input[type="checkbox"]');
+
+      expect(checkboxOfTaskNotDone).not.toBeChecked();
+    });
+
+    it("should render done tasks with checkbox checked", () => {
+      const props = {
+        ...defaultProps,
+        tasks: [taskDone],
+      };
+      const taskDoneId = props.tasks[0].id;
+
+      render(<Todo {...props} />);
+      const checkboxOfTaskDone = screen.getByTestId(taskDoneId).querySelector('input[type="checkbox"]');
+
+      expect(checkboxOfTaskDone).toBeChecked();
+    });
+
+    describe("when task is clicked", () => {
+      it("should call onTaskClick callback with task's ID", () => {
+        const props = {
+          ...defaultProps,
+          tasks,
+          onTaskClick: jest.fn(),
+        };
+        const taskIdToToggle = props.tasks[0].id;
+        render(<Todo {...props} />);
+        const taskToToggle = screen.getByTestId(taskIdToToggle);
+
+        fireEvent.click(taskToToggle);
+
+        expect(props.onTaskClick).toHaveBeenCalledWith(taskIdToToggle);
+      });
+    });
+  });
+});
---
+++ todo/ui/todo.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useRef, FC } from "react";
+import { TaskVM } from "../models/taskVM";
+import styles from "./todo.module.css";
+
+export interface TodoProps {
+  onFormSubmit: (taskDescription: string) => void;
+  tasks: ReadonlyArray<TaskVM>;
+  onTaskClick: (id: TaskVM["id"]) => void;
+}
+
+export const Todo: FC<TodoProps> = ({ onFormSubmit, tasks, onTaskClick }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const submitForm = async () => await onFormSubmit(inputRef.current?.value as string);
+  const resetForm = () => formRef.current?.reset();
+  const focusOnInput = () => inputRef.current?.focus();
+
+  useEffect(focusOnInput, []);
+
+  return (
+    <section className={styles.container}>
+      <h1>TODOs</h1>
+      <form
+        className={styles.form}
+        onSubmit={async (event) => {
+          event.preventDefault();
+          await submitForm();
+          resetForm();
+          focusOnInput();
+        }}
+        ref={formRef}
+      >
+        <span>Enter a task:</span>
+        <input className={styles.taskName} ref={inputRef} name="taskName" placeholder="What do you have to do?" />
+        <button className={styles.submitButton} type="submit">
+          Add
+        </button>
+      </form>
+      <ul className={styles.taskList}>
+        {tasks.map(({ id, description, done }) => (
+          <li key={id} data-testid={id} onClick={() => onTaskClick(id)}>
+            <label className={styles.taskLabel}>
+              <input type="checkbox" checked={done} readOnly />
+              <span className={done ? styles.taskDone : ""}>{description}</span>
+            </label>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
